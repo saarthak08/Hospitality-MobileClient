f6132512048f9feb6beecd9504a495b29539fa6c
Rishabh <princerishabh.sharma400@gmail.com>
2020-04-11 22:24:23 +0530

    Input for distance

--- lib/src/providers/location_provider.dart
+++ lib/src/providers/location_provider.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 
-class LocationProvider extends ChangeNotifier{
+class LocationProvider extends ChangeNotifier {
   LocationData _location;
+  int _hospitalDistance;
 
   LocationData get getLocation {
     return _location;
   }
 
-  set setLocation(LocationData data){
-    _location=data;
+  set setLocation(LocationData data) {
+    _location = data;
+    notifyListeners();
+  }
+
+  set setHospitalDistance(int data) {
+    _hospitalDistance = data;
     notifyListeners();
   }
 }
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hospital_service/src/helpers/current_location.dart';
+import 'package:hospital_service/src/helpers/dimensions.dart';
 import 'package:hospital_service/src/providers/location_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:hospital_service/src/resources/network/network_repository.dart';
+import '../helpers/dimensions.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key, this.title}) : super(key: key);
@@ -17,6 +19,22 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   LocationProvider locationProvider;
 
+  final TextStyle dropdownMenuItem =
+      TextStyle(color: Colors.black, fontSize: 18);
+  int distance = null;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  void _submitForm() async {
+    _formKey.currentState.save();
+    setState(() {
+      locationProvider.setHospitalDistance = distance;
+      print('done');
+    });
+    // Timer(Duration(seconds: 3), () {
+    // });
+  }
+
   @override
   Widget build(BuildContext context) {
     locationProvider = Provider.of<LocationProvider>(context);
@@ -24,21 +42,113 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: Text('HomeScreen'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Push button to get your current Location:',
-            ),
-            Text(
-              'Lets Go',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              colors: [Colors.blue.shade300, Colors.blue.shade500]),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                height: getDeviceHeight(context) * 0.20,
+                width: getDeviceWidth(context) * 0.20,
+                child: Image.asset('assets/img/splash_bg.png'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(
+                    Icons.edit,
+                    color: Colors.white70,
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                    'Input distance for nearby hospital',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white70),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: getDeviceHeight(context) * 0.1,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Material(
+                  elevation: 5.0,
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
+                  child: Form(
+                    key: _formKey,
+                    child: TextFormField(
+                      cursorColor: Theme.of(context).primaryColor,
+                      style: dropdownMenuItem,
+                      decoration: InputDecoration(
+                        hintText: "Search by distance in km",
+                        hintStyle:
+                            TextStyle(color: Colors.black38, fontSize: 16),
+                        prefixIcon: IconButton(
+                            icon: Icon(
+                              Icons.search,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                            onPressed: () {
+                              _submitForm();
+                              _formKey.currentState.reset();
+                              FocusScope.of(context).requestFocus(FocusNode());
+                            }),
+                        border: InputBorder.none,
+                        contentPadding:
+                            EdgeInsets.symmetric(horizontal: 25, vertical: 13),
+                      ),
+                      keyboardType: TextInputType.phone,
+                      onChanged: (String value) {
+                        distance = int.parse(value);
+                      },
+                      onSaved: (String value) {
+                        distance = int.parse(value);
+                      },
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25)),
+                splashColor: Colors.white,
+                color: Colors.white,
+                child: Container(
+                  width: getViewportWidth(context) * 0.35,
+                  height: getViewportHeight(context) * 0.06,
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Search',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Theme.of(context).primaryColor,
+                      fontFamily: "Ubuntu",
+                      fontSize: getViewportHeight(context) * 0.025,
+                    ),
+                  ),
+                ),
+                textColor: Colors.white,
+                onPressed: () {
+                  _submitForm();
+                  _formKey.currentState.reset();
+                  FocusScope.of(context).requestFocus(FocusNode());
+                },
+              ),
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
         onPressed: () async {
           await getLocation().then((value) async {
             if (value == null) {
@@ -60,7 +170,10 @@ class _HomeScreenState extends State<HomeScreen> {
           });
         },
         tooltip: 'Increment',
-        child: Icon(Icons.map),
+        child: Icon(
+          Icons.map,
+          color: Theme.of(context).primaryColor,
+        ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
