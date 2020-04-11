26b1ca8d34f868d7b4acd38fe2abb09024dcdf7c
Saarthak Gupta <saarthakgupta08@gmail.com>
2020-04-11 22:18:26 +0530

    improve map view

---
+++ lib/src/providers/hospital_list_provider.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:hospital_service/src/models/hospital.dart';
+
+class HospitalListProvider extends ChangeNotifier{
+  List<Hospital> _hospitals;
+
+   List<Hospital> get getHospitalsList {
+    return _hospitals;
+  }
+
+  set setHospitalLists(List<Hospital> _hospitals){
+    this._hospitals=_hospitals;
+    notifyListeners();
+  }
+}
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hospital_service/src/helpers/current_location.dart';
+import 'package:hospital_service/src/models/hospital.dart';
+import 'package:hospital_service/src/providers/hospital_list_provider.dart';
 import 'package:hospital_service/src/providers/location_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:hospital_service/src/resources/network/network_repository.dart';
@@ -16,10 +18,13 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   LocationProvider locationProvider;
+  HospitalListProvider hospitalListProvider;
 
   @override
   Widget build(BuildContext context) {
     locationProvider = Provider.of<LocationProvider>(context);
+    hospitalListProvider = Provider.of<HospitalListProvider>(context);
+    
     return Scaffold(
       appBar: AppBar(
         title: Text('HomeScreen'),
@@ -47,6 +52,15 @@ class _HomeScreenState extends State<HomeScreen> {
                   toastLength: Toast.LENGTH_SHORT,
                   gravity: ToastGravity.BOTTOM);
             } else {
+                  Hospital h=Hospital();
+                  h.setDistance=123;
+                  h.setEmail="av";
+                  h.setName="Gandhi Eye";
+                  h.setLatitude=27.8894;
+                  h.setLongitude=78.0834;
+                  List<Hospital> hospitals = List<Hospital>();
+                  hospitals.add(h);
+                  hospitalListProvider.setHospitalLists=hospitals;
               Fluttertoast.showToast(
                   msg:
                       "Latitude: ${value.latitude}\nLongitude: ${value.longitude}",
@@ -55,6 +69,7 @@ class _HomeScreenState extends State<HomeScreen> {
               locationProvider.setLocation = value;
               await getNetworkRepository.sendCurrentLocation(
                   latitude: value.latitude, longitude: value.longitude);
+              
               Navigator.pushNamed(context, "/map");
             }
           });
